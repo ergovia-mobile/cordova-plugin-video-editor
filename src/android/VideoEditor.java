a25dc76e01d251d2cca537d8bcac61e4356ddc9f
ehmm <ehmm@ergovia.de>
2016-05-12 15:25:16 +0200

    Extracted ffmpeg

--- src/android/VideoEditor.java
+++ src/android/VideoEditor.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
-import org.ffmpeg.android.ShellUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
